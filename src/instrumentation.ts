19c93bd18908a576334449bfa89be59711aba7ed
Vladimir Akritsky <leninlin@gmail.com>
2022-10-22 23:09:35 +0200

    Add plugin from public pullrequest

---
+++ src/instrumentation.ts
@@ -0,0 +1,400 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  diag,
+  propagation,
+  trace,
+  context,
+  SpanKind,
+  SpanStatusCode,
+  Context,
+  Span,
+  ROOT_CONTEXT,
+} from "@opentelemetry/api";
+import {
+  InstrumentationBase,
+  InstrumentationNodeModuleDefinition,
+  isWrapped,
+} from "@opentelemetry/instrumentation";
+import { SemanticAttributes } from "@opentelemetry/semantic-conventions";
+import type * as Nats from "nats";
+import { NatsInstrumentationConfig } from "./types";
+import * as utils from "./utils";
+import { VERSION } from "./version";
+
+interface NatsHelpers {
+  /** Tracks which subjects are reply addresses */
+  replySubjects: Set<string>;
+  headers?: () => Nats.MsgHdrs;
+}
+
+/**
+ * Nats instrumentation for Opentelemetry
+ */
+export class NatsInstrumentation extends InstrumentationBase<typeof Nats> {
+  constructor(protected override _config: NatsInstrumentationConfig = {}) {
+    super("@opentelemetry/instrumentation-nats", VERSION, _config);
+    this._natsHelpers = {
+      replySubjects: new Set(),
+    };
+  }
+
+  _natsHelpers: NatsHelpers;
+
+  init(): InstrumentationNodeModuleDefinition<typeof Nats> {
+    return new InstrumentationNodeModuleDefinition<typeof Nats>(
+      "nats",
+      ["2.*"],
+      (moduleExports, moduleVersion) => {
+        diag.debug(`Applying nats patch for nats@${moduleVersion}`);
+        const { headers } = moduleExports;
+        // Grab helpers from nats moduleExports for wrappings in the future
+        this._natsHelpers.headers = headers;
+        this.ensureWrapped(
+          moduleVersion,
+          moduleExports,
+          "connect",
+          this.wrapConnect.bind(this)
+        );
+        return moduleExports;
+      },
+      (moduleExports, moduleVersion) => {
+        if (moduleExports === undefined) return;
+        this._unwrap(moduleExports, "connect");
+        diag.debug(`Removing nats patch for nats@${moduleVersion}`);
+      }
+    );
+  }
+
+  private wrapConnect(originalFunc: typeof import("nats").connect) {
+    const instrumentation = this;
+    const traps = {
+      get: function get(target: Nats.NatsConnection, prop: string) {
+        switch (prop) {
+          case "subscribe":
+            return instrumentation.wrapSubscribe(target.subscribe);
+          case "publish":
+            return instrumentation.wrapPublish(target.publish);
+          case "request":
+            return instrumentation.wrapRequest(target.request);
+          default:
+            return (target as any)[prop];
+        }
+      },
+    };
+    return async function connect(
+      opts?: Nats.ConnectionOptions | undefined
+    ): Promise<Nats.NatsConnection> {
+      const nc = await originalFunc(opts);
+      return new Proxy(nc, traps);
+    };
+  }
+
+  private wrapSubscribe(originalFunc: Nats.NatsConnection["subscribe"]) {
+    const instrumentation = this;
+    return function subscribe(
+      this: Nats.NatsConnection,
+      subject: string,
+      opts?: Nats.SubscriptionOptions
+    ): Nats.Subscription {
+      const nc = this;
+      const genSpanAndContextFromMessage = (m: Nats.Msg): [Context, Span] => {
+        // Extract propagation info from nats header
+        const parentContext = propagation.extract(
+          ROOT_CONTEXT,
+          m.headers,
+          utils.natsContextGetter
+        );
+        const span = instrumentation.tracer.startSpan(
+          `${m.subject} process`,
+          {
+            attributes: {
+              ...utils.traceAttrs(nc.info, m),
+              [SemanticAttributes.MESSAGING_OPERATION]: "process",
+              [SemanticAttributes.MESSAGING_DESTINATION_KIND]: "topic",
+            },
+            // If the message has a reply address, assume it's seeking
+            // a response from us
+            kind: m.reply ? SpanKind.SERVER : SpanKind.CONSUMER,
+          },
+          parentContext
+        );
+        const ctx = trace.setSpan(parentContext, span);
+        return [ctx, span];
+      };
+
+      if (opts?.callback) {
+        const originalCallback = opts.callback;
+        opts.callback = function wrappedCallback(
+          err: Nats.NatsError | null,
+          msg: Nats.Msg
+        ) {
+          if (err) {
+            // If there was an error with Nats, bail early
+            originalCallback(err, msg);
+            return;
+          }
+
+          msg = instrumentation.setupMessage(msg, nc);
+          const [ctx, span] = genSpanAndContextFromMessage(msg);
+          try {
+            context.with(ctx, originalCallback, undefined, err, msg);
+            span.setStatus({
+              code: SpanStatusCode.OK,
+            });
+          } catch (err: any) {
+            span.setStatus({
+              code: SpanStatusCode.ERROR,
+              message: err.message,
+            });
+            span.recordException(err);
+            throw err;
+          } finally {
+            instrumentation.cleanupMessage(msg);
+            span.end();
+          }
+        };
+      }
+      const sub = originalFunc.apply(this, [subject, opts]);
+      if (opts?.callback) {
+        // If we have a callback then sub is no longer an async iterator. No
+        // need to wrap it. Just bail early
+        // https://github.com/nats-io/nats.js#async-vs-callbacks
+        return sub;
+      }
+
+      const wrappedInterator = (async function* wrappedInterator() {
+        for await (let m of sub) {
+          m = instrumentation.setupMessage(m, nc);
+          // FixMe: Fix setting context once
+          // https://github.com/open-telemetry/opentelemetry-js-api/pull/123
+          // lands
+          const [_ctx, span] = genSpanAndContextFromMessage(m);
+
+          try {
+            yield m;
+            span.setStatus({
+              code: SpanStatusCode.OK,
+            });
+          } catch (err: any) {
+            span.setStatus({
+              code: SpanStatusCode.ERROR,
+              message: err.message,
+            });
+            span.recordException(err);
+            throw err;
+          } finally {
+            instrumentation.cleanupMessage(m);
+            span.end();
+          }
+        }
+      })();
+
+      Object.assign(wrappedInterator, sub);
+      return wrappedInterator as any;
+    };
+  }
+
+  private wrapRequest(originalFunc: Nats.NatsConnection["request"]) {
+    const instrumentation = this;
+
+    return async function request(
+      this: Nats.NatsConnection,
+      subject: string,
+      data?: Uint8Array,
+      opts?: Nats.RequestOptions
+    ): Promise<Nats.Msg> {
+      const nc = this;
+      // FixMe: "request" is a non-standard operation. Should we use a diffrent
+      // name/format for this span?
+      const span = instrumentation.tracer.startSpan(`${subject} request`, {
+        attributes: {
+          ...utils.baseTraceAttrs(nc.info),
+        },
+        kind: SpanKind.CLIENT,
+      });
+
+      try {
+        const res = await context.with(
+          trace.setSpan(context.active(), span),
+          originalFunc,
+          this,
+          subject,
+          data,
+          opts
+        );
+        span.setStatus({ code: SpanStatusCode.OK });
+        return res;
+      } catch (err: any) {
+        span.recordException(err);
+        span.setStatus({ code: SpanStatusCode.ERROR, message: err.message });
+        throw err;
+      } finally {
+        span.end();
+      }
+    };
+  }
+
+  private wrapPublish(originalFunc: Nats.NatsConnection["publish"]) {
+    const instrumentation = this;
+    return function publish(
+      this: Nats.NatsConnection,
+      subject: string,
+      data: Uint8Array,
+      options?: Nats.PublishOptions
+    ): void {
+      const nc = this;
+      const isTemporaryDestination =
+        instrumentation.isTemporaryDestination(subject);
+      const destination = isTemporaryDestination ? "(temporary)" : subject;
+      const span = instrumentation.tracer.startSpan(`${destination} send`, {
+        attributes: {
+          ...utils.baseTraceAttrs(nc.info),
+          [SemanticAttributes.MESSAGING_DESTINATION_KIND]: "topic",
+          [SemanticAttributes.MESSAGING_DESTINATION]: destination,
+          [SemanticAttributes.MESSAGING_TEMP_DESTINATION]:
+            isTemporaryDestination,
+          [SemanticAttributes.MESSAGING_MESSAGE_PAYLOAD_SIZE_BYTES]: data
+            ? data.length
+            : 0,
+        },
+        kind: SpanKind.PRODUCER,
+      });
+      if (isTemporaryDestination) {
+        span.setAttribute(
+          SemanticAttributes.MESSAGING_CONVERSATION_ID,
+          subject
+        );
+      } else if (options?.reply) {
+        span.setAttribute(
+          SemanticAttributes.MESSAGING_CONVERSATION_ID,
+          options.reply
+        );
+      }
+      const ctx = trace.setSpan(context.active(), span);
+      const h = options?.headers
+        ? options.headers
+        : instrumentation._natsHelpers.headers!();
+      propagation.inject(ctx, h, utils.natsContextSetter);
+
+      try {
+        context.with(ctx, originalFunc, this, subject, data, {
+          ...options,
+          headers: h,
+        });
+        span.setStatus({ code: SpanStatusCode.OK });
+      } catch (err: any) {
+        span.recordException(err);
+        span.setStatus({ code: SpanStatusCode.ERROR, message: err.message });
+        throw err;
+      } finally {
+        span.end();
+      }
+    };
+  }
+
+  private wrapRespond(
+    originalFunc: Nats.Msg["respond"],
+    nc: Nats.NatsConnection
+  ) {
+    const instrumentation = this;
+    return function respond(
+      this: Nats.Msg,
+      data?: Uint8Array | undefined,
+      options?: Nats.PublishOptions | undefined
+    ) {
+      const msg = this;
+      const destination = "(temporary)";
+      const span = instrumentation.tracer.startSpan(`${destination} send`, {
+        attributes: {
+          ...utils.baseTraceAttrs(nc.info),
+          [SemanticAttributes.MESSAGING_DESTINATION_KIND]: "topic",
+          [SemanticAttributes.MESSAGING_DESTINATION]: destination,
+          [SemanticAttributes.MESSAGING_TEMP_DESTINATION]: true,
+          [SemanticAttributes.MESSAGING_MESSAGE_PAYLOAD_SIZE_BYTES]: data
+            ? data.length
+            : 0,
+          [SemanticAttributes.MESSAGING_CONVERSATION_ID]: msg.reply,
+        },
+        kind: SpanKind.PRODUCER,
+      });
+      const ctx = trace.setSpan(context.active(), span);
+      const h = msg.headers
+        ? msg.headers
+        : instrumentation._natsHelpers.headers!();
+      propagation.inject(ctx, h, utils.natsContextSetter);
+
+      try {
+        context.with(ctx, originalFunc, this, data, {
+          ...options,
+          headers: h,
+        });
+        span.setStatus({ code: SpanStatusCode.OK });
+      } catch (err: any) {
+        span.recordException(err);
+        span.setStatus({ code: SpanStatusCode.ERROR, message: err.message });
+        throw err;
+      } finally {
+        span.end();
+      }
+    };
+  }
+
+  private isTemporaryDestination(subject: string) {
+    return this._natsHelpers.replySubjects.has(subject);
+  }
+
+  private setupMessage(msg: Nats.Msg, nc: Nats.NatsConnection): Nats.Msg {
+    const instrumentation = this;
+    if (msg.reply) {
+      // Add this reply subject to tracked list. When/if we then respond
+      // to this reply we will know this is a response rather than
+      // publishing to a presisted subject
+      this._natsHelpers.replySubjects.add(msg.reply);
+    }
+    const traps = {
+      get: function get(target: Nats.Msg, prop: string) {
+        if (prop === "respond") {
+          return instrumentation.wrapRespond(target.respond, nc);
+        }
+        return (target as any)[prop];
+      },
+    };
+
+    return new Proxy(msg, traps);
+  }
+
+  private cleanupMessage(msg: Nats.Msg) {
+    if (msg.reply) {
+      // Remove temp reply addresses from memory
+      this._natsHelpers.replySubjects.delete(msg.reply);
+    }
+  }
+
+  ensureWrapped(
+    moduleVersion: string | undefined,
+    obj: any,
+    methodName: string,
+    wrapper: (original: any) => any
+  ) {
+    diag.debug(`Applying ${methodName} patch for nats@${moduleVersion}`);
+    if (isWrapped(obj[methodName])) {
+      this._unwrap(obj, methodName);
+    }
+    this._wrap(obj, methodName, wrapper);
+  }
+}
---
+++ test/nats.test.ts
@@ -0,0 +1,368 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Eslint Disable Explain: Only used in tests
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import { TextEncoder, TextDecoder } from "util";
+import {
+  SpanStatusCode,
+  context,
+  SpanKind,
+  trace,
+  propagation,
+} from "@opentelemetry/api";
+import { HttpTraceContextPropagator } from "@opentelemetry/core";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import {
+  registerInstrumentationTesting,
+  getTestSpans,
+  resetMemoryExporter,
+  startDocker,
+  cleanUpDocker,
+} from "@opentelemetry/contrib-test-utils";
+import { ReadableSpan } from "@opentelemetry/sdk-trace-base";
+import * as assert from "assert";
+import * as natsTypes from "nats";
+import { NatsInstrumentation } from "../src";
+import { SemanticAttributes } from "@opentelemetry/semantic-conventions";
+
+const instrumentation = registerInstrumentationTesting(
+  new NatsInstrumentation()
+);
+
+const CONFIG = {
+  host: process.env.OPENTELEMETRY_NATS_HOST || "0.0.0.0",
+  port: parseInt(process.env.OPENTELEMETRY_NATS_PORT || "4222", 10),
+};
+
+const DEFAULT_ATTRIBUTES = {
+  [SemanticAttributes.MESSAGING_SYSTEM]: "nats",
+  [SemanticAttributes.MESSAGING_PROTOCOL]: "nats",
+  [SemanticAttributes.NET_PEER_NAME]: CONFIG.host,
+  [SemanticAttributes.NET_PEER_PORT]: `${CONFIG.port}`,
+};
+
+const sortByStartTime = (a: ReadableSpan, b: ReadableSpan) => {
+  const aInMs = a.startTime[0] * 1000000 + a.startTime[1] / 1000;
+  const bInMs = b.startTime[0] * 1000000 + b.startTime[1] / 1000;
+  return aInMs - bInMs;
+};
+
+const URL = `${CONFIG.host}:${CONFIG.port}`;
+
+describe("nats@2.x", () => {
+  const provider = new NodeTracerProvider();
+  const shouldTestLocal = process.env.RUN_NATS_TESTS_LOCAL;
+  const shouldTest = process.env.RUN_NATS_TESTS || shouldTestLocal;
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  let nats: typeof natsTypes;
+  let nc: natsTypes.NatsConnection;
+
+  before(function () {
+    // needs to be "function" to have MochaContext "this" context
+    if (!shouldTest) {
+      // this.skip() workaround
+      // https://github.com/mochajs/mocha/issues/2683#issuecomment-375629901
+      this.test!.parent!.pending = true;
+      this.skip();
+    }
+
+    if (shouldTestLocal) {
+      //@ts-ignore
+      startDocker("nats");
+    }
+
+    // diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+    nats = require("nats");
+  });
+
+  after(() => {
+    if (shouldTestLocal) {
+      //@ts-ignore
+      cleanUpDocker("nats");
+    }
+  });
+
+  beforeEach(async () => {
+    propagation.setGlobalPropagator(new HttpTraceContextPropagator());
+    nc = await nats.connect({ servers: URL });
+  });
+
+  afterEach((done) => {
+    resetMemoryExporter();
+    propagation.disable();
+    nc.drain().then(done, done);
+  });
+
+  it("should have correct module name", () => {
+    assert.strictEqual(
+      instrumentation.instrumentationName,
+      "@opentelemetry/instrumentation-nats"
+    );
+  });
+
+  describe("#publish", () => {
+    it("should create a span", () => {
+      const parentSpan = provider.getTracer("default").startSpan("test span");
+      context.with(trace.setSpan(context.active(), parentSpan), () => {
+        nc.publish("test");
+        const spans = getTestSpans();
+        assertSpans(spans, [
+          {
+            subject: "test",
+            op: "send",
+            kind: SpanKind.PRODUCER,
+            status: { code: SpanStatusCode.OK },
+            parentSpan,
+          },
+        ]);
+      });
+    });
+  });
+
+  describe("#subscribe (callbacks)", () => {
+    it("should create a consumer span", async () => {
+      const parentSpan = provider.getTracer("default").startSpan("test span");
+      let resolve: (v?: unknown) => void;
+      const p = new Promise((r) => {
+        resolve = r;
+      });
+      context.with(trace.setSpan(context.active(), parentSpan), () => {
+        nc.subscribe("test", {
+          max: 1,
+          callback: (err, _msg) => {
+            assert.strictEqual(err, null);
+            resolve();
+          },
+        });
+        nc.publish("test");
+      });
+
+      await p;
+      const spans = getTestSpans();
+      assertSpans(spans, [
+        {
+          subject: "test",
+          op: "send",
+          kind: SpanKind.PRODUCER,
+          parentSpan,
+        },
+        {
+          subject: "test",
+          op: "process",
+          kind: SpanKind.CONSUMER,
+          parentSpan: spans[0],
+        },
+      ]);
+    });
+  });
+
+  describe("#subscribe (async interator)", () => {
+    it("should create a consumer span", async () => {
+      const parentSpan = provider.getTracer("default").startSpan("test span");
+      await context.with(
+        trace.setSpan(context.active(), parentSpan),
+        async () => {
+          const sub = nc.subscribe("test", {
+            max: 1,
+          });
+          nc.publish("test");
+          for await (const _m of sub) {
+          }
+        }
+      );
+      const spans = getTestSpans();
+      assertSpans(spans, [
+        {
+          subject: "test",
+          op: "send",
+          kind: SpanKind.PRODUCER,
+          parentSpan,
+        },
+        {
+          subject: "test",
+          op: "process",
+          kind: SpanKind.CONSUMER,
+          parentSpan: spans[0],
+        },
+      ]);
+    });
+  });
+
+  describe("#request and #respond (callbacks)", () => {
+    it("creats connected spans for the request/response flow", async () => {
+      const parentSpan = provider.getTracer("default").startSpan("test span");
+      let resolve: (v?: unknown) => void;
+      const p = new Promise((r) => {
+        resolve = r;
+      });
+      const res = await context.with(
+        trace.setSpan(context.active(), parentSpan),
+        () => {
+          nc.subscribe("test", {
+            max: 1,
+            callback: (err, msg) => {
+              assert.strictEqual(err, null);
+              msg.respond(encoder.encode("ack"));
+              resolve();
+            },
+          });
+          return nc.request("test");
+        }
+      );
+
+      await p;
+      assert.ok(res, "received response");
+      assert.strictEqual(
+        decoder.decode(res.data),
+        "ack",
+        "got correct response"
+      );
+      const spans = [...getTestSpans()].sort(sortByStartTime);
+      assertSpans(spans, [
+        {
+          subject: "test",
+          op: "request",
+          kind: SpanKind.CLIENT,
+          parentSpan,
+        },
+        {
+          subject: "test",
+          op: "send",
+          kind: SpanKind.PRODUCER,
+          parentSpan: spans[0],
+        },
+        {
+          subject: "test",
+          op: "process",
+          kind: SpanKind.SERVER,
+          parentSpan: spans[1],
+        },
+        {
+          subject: "(temporary)",
+          op: "send",
+          kind: SpanKind.PRODUCER,
+          parentSpan: spans[2],
+        },
+      ]);
+    });
+  });
+
+  // Fixme: Uncomment this test once async interators properly persist context
+  // when calling Msg#respond
+  describe.skip("#request and #respond (async interator)", () => {
+    it("creats connected spans for the request/response flow", async () => {
+      const parentSpan = provider.getTracer("default").startSpan("test span");
+      const res = await context.with(
+        trace.setSpan(context.active(), parentSpan),
+        async () => {
+          const sub = nc.subscribe("test", {
+            max: 1,
+          });
+          const resP = nc.request("test");
+          for await (const m of sub) {
+            m.respond(encoder.encode("ack"));
+          }
+          return resP;
+        }
+      );
+      assert.ok(res, "received response");
+      assert.strictEqual(
+        decoder.decode(res.data),
+        "ack",
+        "got correct response"
+      );
+      const spans = [...getTestSpans()].sort(sortByStartTime);
+      assertSpans(spans, [
+        {
+          subject: "test",
+          op: "request",
+          kind: SpanKind.CLIENT,
+          parentSpan,
+        },
+        {
+          subject: "test",
+          op: "send",
+          kind: SpanKind.PRODUCER,
+          parentSpan: spans[0],
+        },
+        {
+          subject: "test",
+          op: "process",
+          kind: SpanKind.SERVER,
+          parentSpan: spans[1],
+        },
+        {
+          subject: "(temporary)",
+          op: "send",
+          kind: SpanKind.PRODUCER,
+          parentSpan: spans[2],
+        },
+      ]);
+    });
+  });
+});
+
+const assertSpans = (actualSpans: any[], expectedSpans: any[]) => {
+  assert(Array.isArray(actualSpans), "Expected `actualSpans` to be an array");
+  assert(
+    Array.isArray(expectedSpans),
+    "Expected `expectedSpans` to be an array"
+  );
+  assert.strictEqual(
+    actualSpans.length,
+    expectedSpans.length,
+    "Expected span count different from actual"
+  );
+  actualSpans.forEach((span, idx) => {
+    const expected = expectedSpans[idx];
+    if (expected === null) return;
+    try {
+      assert.notStrictEqual(span, undefined);
+      assert.notStrictEqual(expected, undefined);
+      const [spanSubject, spanOp] = span.name.split(" ", 2);
+      assert.strictEqual(spanOp, expected.op, "span name ends with op");
+      assert.strictEqual(
+        spanSubject,
+        expected.subject,
+        "span name starts with subject"
+      );
+      assert.strictEqual(span.kind, expected.kind, "SpanKind matches");
+      for (const attr in DEFAULT_ATTRIBUTES) {
+        assert.strictEqual(
+          span.attributes[attr],
+          DEFAULT_ATTRIBUTES[attr],
+          `${attr} is correct`
+        );
+      }
+      assert.deepEqual(
+        span.status,
+        expected.status || { code: SpanStatusCode.OK },
+        "span status match"
+      );
+      assert.strictEqual(
+        span.parentSpanId,
+        expected.parentSpan?.spanContext().spanId,
+        "span parent id matches"
+      );
+    } catch (e) {
+      e.message = `At span[${idx}]: ${e.message}`;
+      throw e;
+    }
+  });
+};
